5f53d772c467211c1c110b6a42b229b15cf8a22a
Ayan <anarbajaan1@gmail.com>
2025-02-04 15:50:23 +0500

    added 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Button from '../UI/myButton'
+import { useNavigate } from 'react-router-dom'
 
 const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
     <div className='not_found'>
-      <h2>404 | Page not found</h2><br />
-      <Button />
+      <h2>404 | Page not found</h2>
+      <Button onClick={() => navigate('/')} margin="20px" className='profile_btn' borderRadius='30px' bgcolor='black' text="Go to Home page" />
     </div>
   )
 }
